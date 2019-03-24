6d860f7e1b5e54d0e62bd27911b003a523051c31
pangtouyusos <48876466+pangtouyusos@users.noreply.github.com>
2019-03-24 15:18:45 +0800

    Add files via upload

---
+++ precache-manifest.3d1396d1b4d6dd22fb068257ebe6fd27.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bd0050d8408884359f0836e9ae89cd0",
+    "url": "/static/media/snowBtn.9bd0050d.png"
+  },
+  {
+    "revision": "1554f092ad592aad23d9",
+    "url": "/static/css/main.a1abde92.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "39dcf1eb534e7d0408b0",
+    "url": "/static/js/2.2738894e.chunk.js"
+  },
+  {
+    "revision": "d4e27712a819ab276166cb2feb4175fb",
+    "url": "/static/media/oceanImg.d4e27712.jpg"
+  },
+  {
+    "revision": "86db7e0746df744c16e86cb51eddd3b5",
+    "url": "/static/media/volcanoImg.86db7e07.png"
+  },
+  {
+    "revision": "3612a1f1457491062972946f4d372d46",
+    "url": "/static/media/on.3612a1f1.png"
+  },
+  {
+    "revision": "a88dd3be7917b45aba55a7bf823b89bb",
+    "url": "/static/media/background.a88dd3be.png"
+  },
+  {
+    "revision": "9f08a07542295e828c5ccb9c18cff7d1",
+    "url": "/static/media/start.9f08a075.png"
+  },
+  {
+    "revision": "9e291a11879ce0736d2fa7f5cc6001da",
+    "url": "/static/media/settings.9e291a11.png"
+  },
+  {
+    "revision": "6334da496a99488fae0fdeab369ca18a",
+    "url": "/static/media/bg.6334da49.png"
+  },
+  {
+    "revision": "5857c1cddfe386f2e4f7dae8103d8386",
+    "url": "/static/media/jungleBtn.5857c1cd.png"
+  },
+  {
+    "revision": "1554f092ad592aad23d9",
+    "url": "/static/js/main.3a66faa7.chunk.js"
+  },
+  {
+    "revision": "816286ae06f89ee3b8a7041c57ef7e11",
+    "url": "/static/media/oceanBtn.816286ae.png"
+  },
+  {
+    "revision": "3cf47f005ecc3adfb6b4bc32f1cbe2c1",
+    "url": "/static/media/volcanoBtn.3cf47f00.png"
+  },
+  {
+    "revision": "7e767ac9239175b842a056fb0e8d52ca",
+    "url": "/static/media/jungleV.7e767ac9.mp4"
+  },
+  {
+    "revision": "1883e25df00d2f138e15aa2ddf1e26e7",
+    "url": "/static/media/jungleV.1883e25d.webm"
+  },
+  {
+    "revision": "9ed8fe55d676cb798fa9c73a4c7f5acb",
+    "url": "/static/media/snowV.9ed8fe55.mp4"
+  },
+  {
+    "revision": "4bb6df625f8e861a165164602b998f0e",
+    "url": "/static/media/snowV.4bb6df62.webm"
+  },
+  {
+    "revision": "419b76822a23138440eaf51dc9185b2f",
+    "url": "/static/media/jungleImg.419b7682.png"
+  },
+  {
+    "revision": "d794e5811cde89d167fc3befb0e0d1b6",
+    "url": "/static/media/snowImg.d794e581.png"
+  },
+  {
+    "revision": "9615da661ebc3ed3340996ef190a3785",
+    "url": "/static/media/back.9615da66.png"
+  },
+  {
+    "revision": "94554ad8c3886a8b08be4226bbe10a99",
+    "url": "/static/media/bg.94554ad8.png"
+  },
+  {
+    "revision": "4c23f06aa16d10ef2f661e5c3f20c633",
+    "url": "/index.html"
+  }
+];
